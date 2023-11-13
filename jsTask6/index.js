5b474004155ec9497a390a9ca3b4565443a74e9d
sudhir04121995 <mechsudharsanan555@gmail.com>
2023-11-13 22:32:53 +0530

    class function

---
+++ jsTask6/index.js
@@ -0,0 +1,46 @@
+class Person {
+    constructor(name, age, address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    displayInfo() {
+        console.log(`Name: ${this.name}, Age: ${this.age}, Address: ${this.address}`);
+    }
+}
+
+
+const person1 = new Person("John Doe", 25, "123 Main St, Cityville");
+
+person1.displayInfo();
+
+
+
+
+class UberPriceCalculator {
+    constructor(distance, duration) {
+      this.baseFare = 150;
+      this.costPerKilometer = 10;
+      this.costPerMinute = 2;
+      this.distance = distance;
+      this.duration = duration;
+    }
+  
+    calculatePrice() {
+      const totalFare = this.baseFare + (this.distance * this.costPerKilometer) + (this.duration * this.costPerMinute);
+      return totalFare;
+    }
+  }
+  
+  
+  const distance = 10; 
+  const duration = 20;
+  
+
+  const uberCalculator = new UberPriceCalculator(distance, duration);
+  
+
+  const price = uberCalculator.calculatePrice();
+  console.log(`The estimated Uber price for a ${distance}-km trip taking ${duration} minutes is ₹${price.toFixed(2)}`);
+  
